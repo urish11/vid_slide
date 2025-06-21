221471337de28fc76822f1d0cf447af7e4df9abe
urish11 <uri1234@gmail.com>
2025-06-21 18:27:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -557,11 +557,12 @@ def create_facebook_ad_new(bg_img_path: str, headline_text1, headline_text2, hea
         st.write("Has mask?", arrows_overlay.mask is not None)
         # arrows_overlay = arrows_overlay.set_mask(
         # arrows_overlay.mask.fx(lambda m: m.to_ImageClip().fl_image(lambda img: (img > 0.95).astype(float))))
-        final_arrow_y = int(0.51 * resolution[1])
-        final_arrow_x = int(0.28 * resolution[0])
+        final_arrow_y = int(0.46 * resolution[1])
+        final_arrow_x = int(0.275 * resolution[0])
         # arrows_overlay = arrows_overlay.rotate(-90, apply_to='mask')
-        arrows_overlay = arrows_overlay.loop(duration=duration)
-        arrows_overlay = arrows_overlay.set_position((final_arrow_x, final_arrow_y )).set_start(5)
+        arrows_overlay = arrows_overlay.loop(duration=duration).resize(0.67)
+
+        arrows_overlay = arrows_overlay.set_position((final_arrow_x, final_arrow_y )).set_start(5.5)
 
         # arrows_overlay = (
         #                     mp.VideoFileClip("arrows_2.mov", has_mask=True)
